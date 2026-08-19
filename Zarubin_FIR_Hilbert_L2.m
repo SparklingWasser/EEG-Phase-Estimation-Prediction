31dfaa282a04f3e78b4a5134a86a048860dea215
SPARK <s.park7532@gmail.com>
2026-08-19 10:56:44 +0200

    Refactor Zarubin_FIR_Hilbert_L2 function implementation

--- Zarubin_FIR_Hilbert_L2.m
+++ Zarubin_FIR_Hilbert_L2.m
@@ -10,14 +10,19 @@ function output = Zarubin_FIR_Hilbert_L2(data, fs, target_freq, cutoff_freq, nSa
 %
 % Every parameter is set up as in the reference
 
+if length(data) ~= size(data, 2)
+    data = data';
+end
+
 data_len = length(data)/fs;
 
 filt_len_in_s = 0.1;
-B = fir1(filt_len_in_s*fs, cutoff_freq/(fs/2));
+filt_order = floor(filt_len_in_s*fs) + mod(floor(filt_len_in_s*fs), 2);
+B = fir1(filt_order, cutoff_freq/(fs/2));
 
 data_filtered = filter(B, 1, data);   % zero-phase filter from literature
-data_filtered_delay_corrected = data_filtered(filt_len_in_s*fs/2+1:end);
-t_delay_corrected = 1/fs:1/fs:data_len-filt_len_in_s/2;
+data_filtered_delay_corrected = data_filtered(filt_order/2+1:end);
+t_delay_corrected = 0:1/fs:data_len - 1/fs - filt_len_in_s/2;
 
 
 phase_train = angle(hilbert(data_filtered_delay_corrected));
@@ -27,15 +32,15 @@ phase_shift = linspace(0, 2*pi, 72+1);  % 5 degree increment
 sin_waves = zeros(length(phase_shift), length(t_delay_corrected));
 L2_dist = zeros(length(phase_shift), 1);
 for i = 1:length(phase_shift)
-    sin_waves(i, :) = sin(2*pi*target_freq*t_delay_corrected+phase_shift(i));
+    sin_waves(i, :) = cos(2*pi*target_freq*t_delay_corrected + phase_shift(i));
     L2_dist(i) = sqrt(sum(abs(angle(hilbert(sin_waves(i, :))) - phase_train).^2));
 end
 
 [~, min_idx] = min(L2_dist);
 
-t_prediction = data_len+1/fs:1/fs:data_len + nSamp_predict/fs;
+% t_prediction = data_len+1/fs:1/fs:data_len + nSamp_predict/fs;
+t_prediction = data_len + 1/fs : 1/fs : data_len + nSamp_predict/fs;
 
-output = sin(2*pi*target_freq*t_prediction + phase_shift(min_idx));
-
+output = cos(2*pi*target_freq*t_prediction + phase_shift(min_idx));
 
 
